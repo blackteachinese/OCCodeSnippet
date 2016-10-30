504a70aadb1fc9f06fed13df157ef174ce41ece2
洪茶 <blacktea.hw@alibaba-inc.com>
2016-10-31 01:20:20 +0800

    修改脚本

---
+++ setup_snippet.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+
+# 创建目录
+
+if [[ ! -d "$HOME/Library/Developer/Xcode/UserData/CodeSnippets" ]]; then
+	#statements
+	#mkdir ~/Library/Developer/Xcode/UserData/CodeSnippets
+	echo 'create CodeSnippets folder'
+fi
+
+LIST='ls ~/Library/Developer/Xcode/UserData/CodeSnippets'
+if [[ -z LIST ]]; then
+	#目录空，直接拷贝过去
+	cp -r `pwd`/CodeSnippets/* ~/Library/Developer/Xcode/UserData/CodeSnippets/
+	echo 'folder is empty copy the snippet to xcode snippet folder'
+else
+	#备份再拷贝
+	OLDPATH='~/Library/Developer/Xcode/UserData/CodeSnippetsOld/'
+	mkdir ~/Library/Developer/Xcode/UserData/CodeSnippetsOld/
+	echo 'create  folder'$OLDPATH
+	mv ~/Library/Developer/Xcode/UserData/CodeSnippets/ ~/Library/Developer/Xcode/UserData/CodeSnippetsOld/
+	echo 'backup the old xcode snippets to '$OLDPATH
+	cp -r `pwd`/CodeSnippets/ ~/Library/Developer/Xcode/UserData/CodeSnippets/
+	echo
+fi
--- setup_snippet_link.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
-/
-
-SRC_HOME='pwd'
-ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
-echo 'link finish'
